1ecb08f17e825e882dc2fcea0f65b8bc4a1f3078
kazimgoksel.kalyoncu <kazimgoksel.kalyoncu@bilgeadamboost.com>
2024-05-21 10:06:55 +0300

    closes #4 closes #8

--- Ank16Shopping/NTier/Shopping.DAL/DataContext/ShoppingDbContext.cs
+++ Ank16Shopping/NTier/Shopping.DAL/DataContext/ShoppingDbContext.cs
@@ -19,5 +19,54 @@ namespace Shopping.DAL.DataContext
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //Admin User Add
+
+            string admin = "Admin";
+            string mail = admin + "@mail.com";
+
+            var hasher = new PasswordHasher<AppUser>();
+
+            builder.Entity<AppUser>()
+                   .HasData(new AppUser{
+                       Id = 1,
+                       Name = admin,
+                       Surname = admin,
+                       UserName = admin,
+                       NormalizedUserName = admin.ToUpper(),
+                       Email = mail,
+                       NormalizedEmail = mail.ToUpper(),
+                       BirthDate = new DateOnly(2000,1,1),
+                       Gender = Common.Gender.None,
+                       EmailConfirmed = true,
+                       PhoneNumberConfirmed = true,
+                       PhoneNumber = "-",
+                       PasswordHash = hasher.HashPassword(null, "Az*123456")
+                   });
+
+
+            //Admin Role Add
+
+            builder.Entity<IdentityRole<int>>()
+                   .HasData(new IdentityRole<int>
+                   {
+                       Id = 1,
+                       Name = admin,
+                       NormalizedName = admin.ToUpper()
+                   });
+
+            //User To Role Add
+
+            builder.Entity<IdentityUserRole<int>>()
+                   .HasData(new IdentityUserRole<int>
+                   {
+                       UserId = 1,
+                       RoleId = 1
+                   });
+        }
     }
 }
