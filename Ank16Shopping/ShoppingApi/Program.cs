b945a3a17ffd73d26a92f8fa5bf19157aaff835f
gokselBaBoost <kazimgoksel.kalyoncu@bilgeadamboost.com>
2024-06-05 23:35:51 +0300

    Swager UI Header eklemesi

--- Ank16Shopping/ShoppingApi/Program.cs
+++ Ank16Shopping/ShoppingApi/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Shopping.BLL.Managers.Concrete;
 using Shopping.DAL.DataContext;
 using Shopping.DAL.Repositories.Concrete;
@@ -11,6 +12,7 @@ using Shopping.DAL.Services.Concrete;
 using Shopping.Entities.Concrete;
 using Shopping.Services.Mail;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -62,9 +64,73 @@ builder.Services.AddAuthentication(opt =>
                     };
                 });
 
+builder.Services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
+
+var securityScheme = new OpenApiSecurityScheme()
+{
+    Name = "Authentication",
+    Type = SecuritySchemeType.Http,
+    Scheme = "Bearer",
+    BearerFormat = "JWT",
+    In = ParameterLocation.Header,
+    Description = "ANK16 JWT Token",
+};
+
+var securityReq = new OpenApiSecurityRequirement()
+{
+    {
+         new OpenApiSecurityScheme
+        {
+            Reference= new OpenApiReference
+            {
+                Type=ReferenceType.SecurityScheme,
+                Id="Bearer"
+            }
+
+        },
+         new string[] { }
+    }
+
+
+};
+
+
+var contact = new OpenApiContact()
+{
+    Name = "ANK16 Sınıfı",
+    Email = "ank16@bilgeadam.com",
+    Url = new Uri("https://www.bilgeadam.com")
+};
+
+var license = new OpenApiLicense()
+{
+    Name = "Free",
+    Url = new Uri("https://www.bilgeadam.com")
+};
+
+var info = new OpenApiInfo()
+{
+    Version = "1.0",
+    Title = "JWT uygulaması",
+    Description = "ANK16 Grubu için JWT uygulaması",
+    TermsOfService = new Uri("https://www.bilgeadam.com"),
+    Contact = contact,
+    License = license
+};
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", info);
+    opt.AddSecurityDefinition("Bearer", securityScheme);
+    opt.AddSecurityRequirement(securityReq);
+
+});
+
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<CategoryRepo>();
 builder.Services.AddScoped<CategoryService>();
@@ -101,9 +167,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 //app.UseCors(opt =>
 //{
-//    //opt.AllowAnyOrigin() // * t�m siteler
-//    //   .AllowAnyHeader() // * t�m header istekleri
-//    //   .AllowAnyMethod() // * t�m metotlar (GET,POST,PUT,DELETE)
+//    //opt.AllowAnyOrigin() // * tüm siteler
+//    //   .AllowAnyHeader() // * tüm header istekleri
+//    //   .AllowAnyMethod() // * tüm metotlar (GET,POST,PUT,DELETE)
 //    //   .Build();
 
 //    // https://localhost:7021
